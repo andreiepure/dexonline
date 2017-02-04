7d913ac2760c25f98cb3c2c88fe76100cad0d5c2
Andrei Epure <epure.andrei@gmail.com>
2017-02-04 17:55:15 +0000

    moved db creation first to ask password first

--- tools/setup/setup-scotchbox.sh
+++ tools/setup/setup-scotchbox.sh
@@ -3,12 +3,12 @@
 # Important: The script must be ran with sudo rights
 # How to setup the VM: https://git.io/vDcOb
 
+mysql -u root -p  -e "create database dexonline character set utf8" scotchbox
 mkdir /var/www/public
 cd /var/www/public
 git clone https://github.com/dexonline/dexonline dexonline
 cd dexonline
 /bin/bash tools/setup.sh
-mysql -u root -p  -e "create database dexonline character set utf8" scotchbox
 wget -O /tmp/dex-database.sql.gz https://dexonline.ro/static/download/dex-database.sql.gz
 zcat /tmp/dex-database.sql.gz | mysql -u root -p dexonline
 php tools/migration.php
